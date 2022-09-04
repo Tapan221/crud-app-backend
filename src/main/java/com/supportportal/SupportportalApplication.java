bca070d512ccfcff88b9812aa1738c1637043fcf
Tapan221 <tapanachary.44@gmail.com>
2022-09-04 19:24:42 +0530

    solved CORS Problem

--- src/main/java/com/supportportal/SupportportalApplication.java
+++ src/main/java/com/supportportal/SupportportalApplication.java
@@ -1,9 +1,15 @@
 package com.supportportal;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class SupportportalApplication {
@@ -15,5 +21,33 @@ public class SupportportalApplication {
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+		configuration.setAllowCredentials(true);
+		configuration.setAllowedHeaders(Arrays.asList(
+				"Access-Control-Allow-Headers", 
+				"Access-Control-Allow-Origin",
+				"Access-Control-Request-Method",
+				"Access-Control-Request-Headers", 
+				"Origin", 
+				"Cache-Control",
+				"Content-Type",
+				"Jwt-Token",
+				"Authorization"));
+		configuration.setExposedHeaders(Arrays.asList(
+				"Origin", 
+				"Cache-Control",
+				"Content-Type",
+				"Jwt-Token",
+				"Authorization"));
+		configuration.setAllowedMethods(Arrays.asList("DELETE", "GET", "POST", "PATCH", "PUT"));
+		
+		source.registerCorsConfiguration("/**", configuration);
+		return new CorsFilter(source);
 	}
 }
